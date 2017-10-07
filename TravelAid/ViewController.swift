b933daf46e085bb6a62f61e25a43a14ee0188adc
ro0t <adam@hundaskra.is>
2017-10-07 23:47:16 +0000

    Initial commit

---
+++ TravelAid/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  TravelAid
+//
+//  Created by Adam Viðarsson on 07/10/2017.
+//  Copyright © 2017 Adam Viðarsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "http://localhost:4200")
+        
+        if let unwrappedURL = url {
+            let request = URLRequest(url: unwrappedURL)
+            let session = URLSession.shared
+            
+            let task = session.dataTask(with: request) { (data, response, error) in
+                
+                if error == nil {
+                    self.webView.loadRequest(request)
+                } else {
+                    print("Error happened.")
+                }
+
+            }
+            
+            task.resume()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
